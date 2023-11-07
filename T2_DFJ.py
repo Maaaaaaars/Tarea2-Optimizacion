0bf96febde18134af640d055237dd72c78a2faf1
Bruno Arce R <brunoarce0@gmail.com>
2023-11-07 20:30:58 -0300

    Arreglo DFJ

--- T2_DFJ.py
+++ T2_DFJ.py
@@ -27,11 +27,10 @@ for i in V:
 for j in V:
     problemaDFJ += pulp.lpSum(x[(i, j)] for i in V if i != j) == 1
 
-S = [set(s) for r in range(2, n) for s in combinations(V, r)]
 
-for s in S:
-    for i in s:
-        problemaDFJ += pulp.lpSum(x[(i, j)] for j in s if i != j) <= len(s) - 1
+for s in range(2, n):
+    for S in combinations(range(n), s):
+        problemaDFJ += pulp.lpSum([x[i, j] for i in S for j in S if i != j]) <= len(S) - 1
 
 # Resolver el problema
 problemaDFJ.solve()
